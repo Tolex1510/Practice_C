74c3d4bebf863c135ffd46c9003047e56023b41b
Bogdan Telegin <93638313+Tolex1510@users.noreply.github.com>
2025-03-05 14:21:59 +0300

    Update task10.c

--- homework2/task10.c
+++ homework2/task10.c
@@ -19,6 +19,6 @@ int main() {
     for (int i = 0; i < number; i++) {
         printf("Course: %s\nCredits: %d\nProfessor: %s\n\n", courses[i].title, courses[i].credits, courses[i].professor);
     }
-
+    free(courses);
     return 0;
 }
